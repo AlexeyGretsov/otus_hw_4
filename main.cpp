b69b4c4bf9373ba2f8430e5b695e93e9d48ee22f
AlexeyGretsov <58134097+AlexeyGretsov@users.noreply.github.com>
2025-04-03 00:01:43 +0300

    Adds file

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 
+#include "sfinae_templates.h"
+
 int main(int argc, char *argv[]) {
   try {
     std::cout << "otus_hw_4 ...\n";
+    std::cout << "print_ip: " << print_ip(1) << "\n";
   } catch (const std::exception &e) {
     std::cerr << e.what() << '\n';
   }
---
+++ sfinae_templates.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <string>
+
+template <typename T>
+std::string print_ip(T val) {
+	return std::to_string(val);
+}
